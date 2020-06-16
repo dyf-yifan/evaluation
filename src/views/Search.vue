2f52b617ba0565d6cc70c08d5b19882c2f135f88
dingyifan <510040515@qq.com>
2020-06-16 17:25:07 +0800

    搜索页面调用接口以及其他页面完善

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,14 +1,23 @@
 <!--  -->
 <template>
-  <div>
-    <h1>Search</h1>
+  <div class="container">
+    <input
+      v-model="keywords"
+      type="text"
+      placeholder="搜索你最感兴趣的经典测评"
+      class="search"
+    />
+    <img class="search-img" :src="require('../assets/sousuo.png')" alt="" />
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      keywords: "",
+      searchList: [],
+    };
   },
 
   components: {},
@@ -17,7 +26,52 @@ export default {
 
   mounted() {},
 
-  methods: {}
+  watch: {
+    keywords: function() {
+      this.serach();
+    },
+  },
+
+  methods: {
+    serach() {
+      this.axios({
+        method: "POST",
+        url: "http://localhost:8080/api/list/home/keywords",
+        headers: {
+          "Content-Type": "Access-Control-Allow-Origin",
+        },
+        params: {
+          keywords: this.keywords,
+        },
+      }).then((res) => {
+        this.searchList = res.data.data;
+        console.log(this.searchList);
+      });
+    },
+  },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: auto;
+}
+::-webkit-input-placeholder {
+  color: rgb(155, 163, 170);
+}
+.search {
+  font-size: 10px;
+  outline: none;
+  width: 70%;
+  height: 30px;
+  margin-top: 20px;
+  margin-left: 20px;
+  border-radius: 20px;
+  border: 1px solid rgb(236, 236, 236);
+  padding-left: 5%;
+  color: rgb(155, 163, 170);
+}
+.search-img {
+  border: 1px solid red;
+}
+</style>
--- src/views/TypeContent.vue
+++ src/views/TypeContent.vue
@@ -1,10 +1,62 @@
-<!--  -->
 <template>
-  <div>
-    <div class="task" v-for="(item, index) in questions" :key="index">
-      <div class="task-question">{{ item.question }}</div>
-      <div class="task-answer">{{ item.content }}</div>
+  <div class="container mar">
+    <div class="task-home">
+      <div
+        class="task mar-top"
+        v-for="(type, index1) in questions"
+        :key="index1"
+      >
+        <div class="task-if" v-if="type.status == 0" v-show="index1 == index">
+          <div class="task-question">{{ type.question }}</div>
+          <div
+            class="task-content"
+            v-for="(choice, index2) in type.list"
+            :key="index2"
+          >
+            <input
+              type="radio"
+              :name="type.homeTypeId"
+              :value="choice.homeChoiceId"
+            />{{ choice.content }}
+          </div>
+        </div>
+
+        <div class="task-if" v-if="type.status == 1" v-show="index1 == index">
+          <div class="task-question">{{ type.question }}</div>
+
+          <div v-for="(choice, index2) in type.list" :key="index2">
+            <input
+              type="radio"
+              :name="type.homeTypeId"
+              :value="choice.homeChoiceId"
+            />{{ choice.content }}
+          </div>
+        </div>
+
+        <div class="task-if" v-if="type.status == 2" v-show="index1 == index">
+          <div class="task-question">{{ type.question }}</div>
+
+          <div v-for="(choice, index2) in type.list" :key="index2">
+            <input type="checkBox" :value="choice.homeChoiceId" />{{
+              choice.content
+            }}
+          </div>
+        </div>
+        <div class="task-if" v-if="type.status == 3" v-show="index1 == index">
+          <div class="task-question">{{ type.question }}</div>
+          <div v-for="(choice, index2) in type.list" :key="index2">
+            <input
+              type="radio"
+              :name="type.homeTypeId"
+              :value="choice.homeChoiceId"
+            />{{ choice.content }}
+          </div>
+        </div>
+      </div>
     </div>
+    <button @click="top()" v-show="front">上一页</button>
+    <button @click="nest()" v-show="behind">下一页</button>
+    <button @click="getCode()" v-show="code">提交</button>
   </div>
 </template>
 <script>
@@ -14,7 +66,12 @@ export default {
       listsContent: [],
       id: this.$route.query.id,
       questions: [],
-      question: []
+      question: [],
+      index: 0,
+      front: false,
+      behind: true,
+      code: false,
+      codeId: []
     };
   },
   components: {},
@@ -37,14 +94,6 @@ export default {
         this.question = this.listsContent.oneList;
         for (let i = 0; i < this.question.length; i++) {
           let start = JSON.stringify(this.question[i].question);
-          // let content = JSON.stringify(this.question[i].content);
-          // for (let j = 0; j < this.question.length; j++) {
-          //   let str = "A";
-          //   content =
-          //     document.writeln(String.fromCharCode(str.charCodeAt() + j)) +
-          //     JSON.stringify(this.question[i].content);
-          //   this.question[j].content = content;
-          // }
           let newS = start.indexOf("：");
           let result = start.substring(newS + 1);
           result = result.substring(0, result.length - 1);
@@ -57,22 +106,64 @@ export default {
     },
     set() {
       alert(this.$route.params.id);
+    },
+    nest() {
+      if (this.index == this.questions.length - 2) {
+        this.behind = false;
+        this.code = true;
+      }
+      this.index++;
+      this.front = true;
+    },
+    top() {
+      if (this.index == 1) {
+        this.front = false;
+      }
+      this.index--;
+      this.behind = true;
+      this.code = false;
+    },
+    getCode() {
+      var oTxt = document.getElementsByTagName("input");
+      var tem = 0;
+      for (var i = 0; i < oTxt.length; i++) {
+        if (oTxt[i].checked) {
+          this.codeId[tem] = oTxt[i].value;
+          tem++;
+        }
+      }
+      this.getResult();
+    },
+    getResult() {
+      alert("成绩请到教务处查询");
     }
   }
 };
 </script>
 <style lang="scss" scoped>
+.container {
+  width: 95%;
+}
+.task-home {
+  width: 100%;
+}
+.mar-top {
+  margin-top: 20px;
+}
 .task {
-  width: 90%;
-  height: auto;
+  width: 100%;
+}
+.task-if {
+  width: 100%;
+}
+input {
+  width: 100%;
+  margin-top: 5px;
+  border: 1px solid red;
+  font-size: 15px;
 }
 .task-question {
   width: 100%;
-  height: auto;
-  border: 1px solid red;
-}
-.task-answer {
-  width: 100%;
-  height: 100px;
+  font-size: 50px;
 }
 </style>
