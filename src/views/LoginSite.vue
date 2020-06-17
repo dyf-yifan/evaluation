e5e6c87870c1fc16e0931328ac3e8bfe2f0546e2
dingyifan <510040515@qq.com>
2020-06-17 10:05:11 +0800

    ModuleSearch页面

--- src/views/LoginSite.vue
+++ src/views/LoginSite.vue
@@ -15,6 +15,8 @@ export default {
 
   mounted() {},
 
+  created() {},
+
   methods: {}
 };
 </script>
--- src/views/YiQingModule.vue
+++ src/views/YiQingModule.vue
@@ -1,17 +1,19 @@
 <!-- 疫情防控专题模板 -->
 <template>
   <div>
-    <h1>YiqingModule</h1>
+    <!-- <h1>YiqingModule</h1>
     <router-link to="/yiqing-module/module-search">module-search</router-link>
     <router-link to="/yiqing-module/survey-content">survey-content</router-link>
-    <router-view></router-view>
+    <router-view></router-view> -->
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      module: []
+    };
   },
 
   components: {},
@@ -20,7 +22,25 @@ export default {
 
   mounted() {},
 
-  methods: {}
+  created() {
+    this.getModule();
+  },
+
+  methods: {
+    getModule() {
+      this.axios({
+        method: "POST",
+        url: "http://localhost:8080/api/list/special",
+        headers: {
+          "Content-Type": "Access-Control-Allow-Origin"
+        },
+        params: {}
+      }).then(res => {
+        this.module = res.data.data;
+        console.log(this.module);
+      });
+    }
+  }
 };
 </script>
 <style lang="scss" scoped></style>
